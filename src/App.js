5af66a17ed9431ce6c43670c22ae7ac51b9b20f5
Suji Park <psjhero94@naver.com>
2022-01-12 19:43:13 +0900

    react-router를 사용하여 url 적용

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
-import { useEffect, useState } from "react";
-import Movie from "./components/Movie";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/movie" element={<Detail />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
