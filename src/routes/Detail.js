8377f9b9d90c65fa31254e9d322ce33d49783257
Suji Park <psjhero94@naver.com>
2022-01-13 18:58:16 +0900

    디테일 페이지에서 파라미터를 가져와 상세 영화 정보를 보여주도록 작성

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,41 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
-    return <h1>Detail</h1>
+  const [loading, setLoading] = useState(true);
+  const { id } = useParams();
+  const [movie, setMovie] = useState([]);
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    console.log(json);
+    setMovie(json.data.movie);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <h1>Detail</h1>
+          <img src={movie.medium_cover_image} alt={movie.title} />
+          <p>title: {movie.title}</p>
+          <p>year: {movie.year}</p>
+          <p>{movie.rating}</p>
+          <ul>
+            {movie.genres.map((g) => (
+              <li key={g}>{g}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
